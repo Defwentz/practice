c99e1ea45c81738d64265933c18ee76973a54e5b
Defwentz <tulongd7@163.com>
2016-01-09 22:18:12 +0800

    18-26

--- 99_list.scm
+++ 99_list.scm
@@ -324,3 +324,179 @@
         (cons first-half
               (cons other-half
                     (quote ())))))))
+
+;; P18 (**) Extract a slice from a list.
+;;     Given two indices, I and K, the slice is the list containing the elements between the I'th and K'th element of the original list (both limits included). Start counting the elements with 1.
+;;     
+;;     Example:
+;;     * (slice '(a b c d e f g h i k) 3 7)
+;;     (C D E F G)
+(define slice
+  (lambda (lat i k)
+    (letrec ([skip (lambda (l count)
+                     (cond
+                      [(zero? (sub1 count)) l]
+                      [else
+                       (skip (cdr l) (sub1 count))]))]
+             [leave (lambda (l count)
+                      (cond
+                       [(zero? count)
+                        (cons (car l)
+                              (quote ()))]
+                       [else (cons (car l)
+                                   (leave (cdr l) (sub1 count)))]))])
+      (leave (skip lat i) (- k i)))))
+(slice '(a b c d e f g h i k) 3 7)
+
+;; P19 (**) Rotate a list N places to the left.
+;;     Examples:
+;;     * (rotate '(a b c d e f g h) 3)
+;;     (D E F G H A B C)
+;;     
+;;     * (rotate '(a b c d e f g h) -2)
+;;     (G H A B C D E F)
+;;     
+;;     Hint: Use the predefined functions length and append, as well as the result of problem P17.
+(define rotate
+  (lambda (lat n)
+    (if (zero? n)
+        lat
+        (let ((two-part (split lat
+                               (if (< n 0)
+                                   (+ (length lat) n)
+                                   n))))
+          (append (cadr two-part)
+                  (car two-part))))))
+
+;; P20 (*) Remove the K'th element from a list.
+;;     Example:
+;;     * (remove-at '(a b c d) 2)
+;;     (A C D)
+(define remove-at
+  (lambda (lat k)
+    (cond
+     [(zero? (sub1 k))
+      (cdr lat)]
+     [else
+      (cons (car lat)
+            (remove-at (cdr lat) (sub1 k)))])))
+
+;; P21 (*) Insert an element at a given position into a list.
+;;     Example:
+;;     * (insert-at 'alfa '(a b c d) 2)
+;;     (A ALFA B C D)
+(define insert-at
+  (lambda (elem lat pos)
+    (let I ((l lat)
+            (count (sub1 pos)))
+      (cond
+       [(zero? count)
+        (cons elem l)]
+       [else (cons (car l)
+                   (I (cdr l) (sub1 count)))]))))
+
+;; P22 (*) Create a list containing all integers within a given range.
+;;     If first argument is smaller than second, produce a list in decreasing order.
+;;     Example:
+;;     * (range 4 9)
+;;     (4 5 6 7 8 9)
+(define range
+  (lambda (bot top)
+    (cond
+     [(= bot top)
+      (cons bot (quote ()))]
+     [else (cons bot
+                 (range (add1 bot) top))])))
+
+;; P23 (**) Extract a given number of randomly selected elements from a list.
+;;     The selected items shall be returned in a list.
+;;     Example:
+;;     * (rnd-select '(a b c d e f g h) 3)
+;;     (E D A)
+;;     
+;;     Hint: Use the built-in random number generator and the result of problem P20.
+(define rnd-select
+  (lambda (lat n)
+    (cond
+     [(zero? n)
+      (quote ())]
+     [else (let ((rnd (add1 (random (length lat)))))
+             (cons (element-at lat rnd)
+                   (rnd-select (remove-at lat rnd)
+                               (sub1 n))))])))
+
+;; P24 (*) Lotto: Draw N different random numbers from the set 1..M.
+;;     The selected numbers shall be returned in a list.
+;;     Example:
+;;     * (lotto-select 6 49)
+;;     (23 1 17 33 21 37)
+;;     
+;;     Hint: Combine the solutions of problems P22 and P23.
+(define lotto-select
+  (lambda (n range-top)
+    (rnd-select (range 1 range-top) n)))
+
+;; P25 (*) Generate a random permutation of the elements of a list.
+;;     Example:
+;;     * (rnd-permu '(a b c d e f))
+;;     (B A D C E F)
+;;     
+;;     Hint: Use the solution of problem P23.
+(define rnd-permu
+  (lambda (l)
+    (cond
+     [(null? l)
+      (quote ())]
+     [else (let ((rnd (add1 (random (length l)))))
+             (cons (element-at l rnd)
+                   (rnd-permu (remove-at l rnd))))])))
+
+;; P26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list
+;;     In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficients). For pure mathematicians, this result may be great. But we want to really generate all the possibilities in a list.
+;;     
+;;     Example:
+;;     * (combination 3 '(a b c d e f))
+;;     ((A B C) (A B D) (A B E) ... )
+(define combination
+  (lambda (n pool)
+    (cond
+     [(zero? n)
+      (quote ())]
+     [(null? pool)
+      (quote ())]
+     [(= n (length pool))
+      (cons pool (quote ()))]
+     [else ((if (= n 1)
+                cons
+                append) (cons-all (car pool)
+                                  (combination (sub1 n) (cdr pool)))
+                (combination n (cdr pool)))])))
+(define cons-all
+  (lambda (new-car l)
+    (if (null? l) (cons new-car l)
+        (let CT ((ll l))
+          (cond
+           [(null? ll)
+            (quote ())]
+           [else (cons (cons new-car
+                             (car ll))
+                       (CT (cdr ll)))])))))
+
+;; P27 (**) Group the elements of a set into disjoint subsets.
+;;     a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? Write a function that generates all the possibilities and returns them in a list.
+;;     
+;;     Example:
+;;     * (group3 '(aldo beat carla david evi flip gary hugo ida))
+;;     ( ( (ALDO BEAT) (CARLA DAVID EVI) (FLIP GARY HUGO IDA) )
+;;     ... )
+;;     
+;;     b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+;;     
+;;     Example:
+;;     * (group '(aldo beat carla david evi flip gary hugo ida) '(2 2 5))
+;;     ( ( (ALDO BEAT) (CARLA DAVID) (EVI FLIP GARY HUGO IDA) )
+;;     ... )
+;;     
+;;     Note that we do not want permutations of the group members; i.e. ((ALDO BEAT) ...) is the same solution as ((BEAT ALDO) ...). However, we make a difference between ((ALDO BEAT) (CARLA DAVID) ...) and ((CARLA DAVID) (ALDO BEAT) ...).
+;;     
+;;     You may find more about this combinatorial problem in a good book on discrete mathematics under the term "multinomial coefficients".
